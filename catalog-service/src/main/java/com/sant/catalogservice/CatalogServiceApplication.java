a014a624e18d464eb3f4ee7033be09a2b467a5ff
santosh <santoshrakh6094@gmail.com>
2024-05-12 17:19:59 +0530

    First commit

---
+++ catalog-service/src/main/java/com/sant/catalogservice/CatalogServiceApplication.java
@@ -0,0 +1,14 @@
+package com.sant.catalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogServiceApplication.class, args);
+
+        System.out.println("hello ");
+    }
+}

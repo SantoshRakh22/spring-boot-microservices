cfbbe74038e701d55e4e4bfe1bf8a4d68889915e
santosh <santoshrakh6094@gmail.com>
2024-05-26 16:07:17 +0530

    second service i.e orderService

---
+++ order-service/src/main/java/com/santosh/ApplicationProperties.java
@@ -0,0 +1,16 @@
+package com.santosh;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "orders")
+public record ApplicationProperties(
+        String orderEventsExchange,
+        String newOrdersQueue,
+        String deliveredOrdersQueue,
+        String cancelledOrdersQueue,
+        String errorOrdersQueue
+
+
+) {
+}
---
+++ order-service/src/main/java/com/santosh/domain/OrdersNotFoundException.java
@@ -0,0 +1,11 @@
+package com.santosh.domain;
+
+public class OrdersNotFoundException extends  RuntimeException{
+    public OrdersNotFoundException(String message){
+        super(message);
+    }
+    public static OrdersNotFoundException forOrderNumber(String orderNumber){
+        return  new OrdersNotFoundException("order with number "+orderNumber);
+    }
+
+}
